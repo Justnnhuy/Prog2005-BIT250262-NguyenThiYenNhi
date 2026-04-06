c4e7457d8360adf9d8a90d1e4eb219079b51d4b3
Justnnhuy <painecinhgei@gmail.com>
2026-04-06 15:24:55 +0700

    Add files via upload

---
+++ Luyện tâp 1/Bài 2.py
@@ -0,0 +1,8 @@
+a=6
+b=7
+tong = a+b
+hieu = a-b
+tich = a*b
+print("tổng 2 số là:", tong)
+print("hiệu 2 số là:", hieu)
+print("tích 2 số là:", tich)
---
+++ Luyện tâp 1/Bài 3;.py
@@ -0,0 +1,20 @@
+
+class Book:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def get_name(self):
+        return self.name
+    def get_price(self):
+        return self.price
+
+    def set_name(self, name):
+        self.name = name
+    def set_price(self, price):
+        self.price = price
+
+
+# ví dụ
+book = Book("Lập trình Python", 150000)
+print("Price:", book.get_price())
